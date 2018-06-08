7c0695cb911b42586db08377c546de738bb80b43
nion8 <andrew-881@yandex.ru>
2018-06-08 15:48:10 +0300

    baza dannyh

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,20 @@
+class ContactsController < ApplicationController
+
+
+  def new
+
+  end
+
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :messge)
+  end
+end
